24119ab13f9c87455b0ebca504fa02d438dd3b96
Alexandr <aleksanderborodoolin@mail.ru>
2025-08-19 15:12:27 +0300

    Delete odd code

--- Compass/Compass/Compass/CustomModels/HeadModel.swift
+++ Compass/Compass/Compass/CustomModels/HeadModel.swift
@@ -24,32 +24,15 @@ class HeadModel: CustomModel {
     
     var depthStencilState: MTLDepthStencilState!
     
-    var vertexDescriptor: MTLVertexDescriptor {
+    static var vertexDescriptor: MTLVertexDescriptor {
         let vertexDescriptor = MTLVertexDescriptor()
         vertexDescriptor.attributes[0].format = .float3
         vertexDescriptor.attributes[0].offset = 0
         vertexDescriptor.attributes[0].bufferIndex = 0
         
-        vertexDescriptor.layouts[0].stride = MemoryLayout<float3>.stride
-        
-        vertexDescriptor.attributes[1].format = .float3
-        vertexDescriptor.attributes[1].offset = 0
-        vertexDescriptor.attributes[1].bufferIndex = 1
-        
-        vertexDescriptor.layouts[1].stride = MemoryLayout<float3>.stride
-        
-        return vertexDescriptor
-    }
-    
-    static var vertexDescriptor: MTLVertexDescriptor {
-        let vertexDescriptor = MTLVertexDescriptor()
-        vertexDescriptor.attributes[0].format = .float4
-        vertexDescriptor.attributes[0].offset = 0
-        vertexDescriptor.attributes[0].bufferIndex = 0
-        
         vertexDescriptor.layouts[0].stride = MemoryLayout<float4>.stride
         
-        vertexDescriptor.attributes[1].format = .float4
+        vertexDescriptor.attributes[1].format = .float3
         vertexDescriptor.attributes[1].offset = 0
         vertexDescriptor.attributes[1].bufferIndex = 1
         
